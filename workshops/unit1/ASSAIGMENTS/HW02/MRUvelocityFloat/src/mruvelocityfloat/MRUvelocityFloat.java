6002b43daa7835f1ea1fdb2ec2fd31610c2df276
Erick0112 <erickespe2001@hotmail.com>
2021-01-10 21:44:20 -0500

    Homework MRU velocity object

---
+++ workshops/unit1/ASSAIGMENTS/HW02/MRUvelocityFloat/src/mruvelocityfloat/MRUvelocityFloat.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mruvelocityfloat;
+
+/**
+ *
+ * @author Pc
+ */
+public class MRUvelocityFloat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+        
+    float dividend_metros; 
+    float divisor_tiempo;
+    float division;
+    // un automivil realiza un viaje de 200000m metros  en un tiempo estimado de 3600 seg  ¿A qué velocidad viaja? 
+    
+    dividend_metros= 200000;
+    divisor_tiempo= 3600;
+   
+      System.out.println("The division of" + dividend_metros + "/" + divisor_tiempo 
+                 + "is esqual to -->" + (dividend_metros / divisor_tiempo)); 
+        
+    }
+    
+}
---
+++ workshops/unit1/ASSAIGMENTS/HW02/MRUvelocityShort/src/mruvelocityshort/MRUvelocityShort.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mruvelocityshort;
+
+/**
+ *
+ * @author Pc
+ */
+public class MRUvelocityShort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+        float dividend_metros; 
+    float divisor_tiempo;
+    float division; 
+//a car makes a trip of 50000m meters in an estimated time of 1000 sec. How fast is it traveling? 
+
+    dividend_metros= 50000;
+    divisor_tiempo= 1000;
+   
+      System.out.println("The division of" + dividend_metros + "/" + divisor_tiempo 
+                 + "is esqual to -->" + (dividend_metros / divisor_tiempo));     
+    
+    }
+    
+}
