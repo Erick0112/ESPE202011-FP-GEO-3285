c0da17fa1b521ca95234af3284700128846b8fcd
Erick0112 <erickespe2001@hotmail.com>
2021-01-13 14:48:35 -0500

    WSO5 Iteración

---
+++ assignments/Unity 1/HW02/MRUvelocityInt/src/mruvelocityint/MRUvelocityInt.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mruvelocityint;
+
+/**
+ *
+ * @author Pc
+ */
+public class MRUvelocityInt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     short dividend_metros; 
+    short divisor_tiempo;
+    short division; 
+//a car makes a trip of 3000 m meters in an estimated time of 100 sec. How fast is it traveling? 
+
+    dividend_metros= 3000;
+    divisor_tiempo= 100;
+   
+      System.out.println("The division of" + dividend_metros + "/" + divisor_tiempo 
+                 + "is esqual to -->" + (dividend_metros / divisor_tiempo));   
+        
+    }
+    
+}
