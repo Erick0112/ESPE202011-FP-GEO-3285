443b20687654fb6cc812db21ca3fb17d5a8d6743
Erick0112 <erickespe2001@hotmail.com>
2021-02-19 01:30:54 -0500

    WS13 Task

--- workshops/Unit2/WS13-Task/src/ws13/task/WS13Task.java
+++ workshops/Unit2/WS13-Task/src/ws13/task/WS13Task.java
@@ -18,144 +18,129 @@ public class WS13Task {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        Scanner input = new Scanner(System.in);
 
-   System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n"
-                + "|             UNIVERSIDAD DE LAS FUERZAS ARMADAS              |\n"
-                + "|                         (ESPE)                              |\n"
-                + "| Programming Fundamentals                                    |\n"
-                + "| Programer: ERICK OCHOA                                      |\n"
-                + "| WS13 TASK                                                  |\n"
-                + "| CALCULATE TAXES                                             |\n"
-                + "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n");
+        printBanner();
         float result;
         float area = 0;
-        float areaofpolygon;
-        float areaofcircle;
-        float formula = 0;
-        float distance;
-        float force;
-        float work;
-        System.out.println("TRAPEZOIDE AREA");
-        result = showTrapezoidArea(area);
-        System.out.println("th area of trapezoid is--->" + result);
-
-        System.out.println("REGULAR POLYGON AREA");
-        areaofpolygon = showRegularPolygonArea(area);
-        System.out.println("the area og regula polygon is -->" + areaofpolygon);
-
-        System.out.println("CIRCLE AREA");
-        areaofcircle = showCircleArea(area);
-        System.out.println("the area of circle is ->" + areaofcircle);
-
-        System.out.println("FOMULA OF DISTANCE MRUV");
-        distance = showFormulaOfDistance(formula);
-        System.out.println("the distance traveled is --> " + distance);
-
-        System.out.println("FORMULA OF FORCE");
-        force = showFomulaOfForce(formula);
-        System.out.println(" the force is -->" + force);
-
-        System.out.println("FORMULA OF WORK");
-        work = showFomulaOfWork(formula);
-        System.out.println(" the work is -->" + work);
-
-    
-    
-    }
-
-    public static float showTrapezoidArea(float area) {
-        Scanner input = new Scanner(System.in);
         float majorbase;
         float minorbase;
         float altitude;
-         System.out.println(" enter major base -->   ");
-        majorbase= input.nextInt();
+        float areaofpolygon;
+        float numberofsides;
+        float sides;
+        float perimeter = 0;
+        float apothem;
+        float areaofcircle;
+        float radio;
+        float distance = 0;
+        float initialvelocity;
+        float time;
+        float aceleration;
+        float force = 0;
+        float mass;
+        float forces;
+        float distances;
+
+        float work = 0;
+
+        System.out.println("TRAPEZOIDE AREA");
+        System.out.println(" enter major base -->   ");
+        majorbase = input.nextInt();
         System.out.println("enter minor base -->  ");
-        minorbase= input.nextInt();
+        minorbase = input.nextInt();
         System.out.println("enter altitude --> ");
         altitude = input.nextInt();
-        area = (( majorbase + minorbase)/2) * altitude;
-        return area;
-    }
-    
-     public static float showRegularPolygonArea(float area){
-        Scanner input = new Scanner(System.in);
-         float numberofsides;
-         float sides;
-         float perimeter;
-         float apothem;
-   
+        result = computeTrapezoidArea(area, majorbase, minorbase, altitude);
+        System.out.println("th area of trapezoid is--->" + result);
 
-         System.out.println("enter number of sides --> ");
-         numberofsides = input.nextInt(); 
-         System.out.println("enter sides -->  ");
-         sides = input.nextFloat();
-         perimeter = numberofsides * sides;
-        
-         System.out.println(" enter the apotthem -->  ");
-         apothem = input.nextFloat();
-         
-         area = (perimeter * apothem)/ 2;
-         return area;
-     }
+        System.out.println("REGULAR POLYGON AREA");
+        System.out.println("enter number of sides --> ");
+        numberofsides = input.nextInt();
+        System.out.println("enter sides -->  ");
+        sides = input.nextFloat();
+        System.out.println(" enter the apotthem -->  ");
+        apothem = input.nextFloat();
+        areaofpolygon = computeRegularPolygonArea(area, numberofsides, sides, perimeter, apothem);
+        System.out.println("the area og regula polygon is -->" + areaofpolygon);
 
-
-   public static float showCircleArea(float area){
-        Scanner input = new Scanner(System.in);
-        float radio;
+        System.out.println("CIRCLE AREA");
         System.out.println("enter the radio --> ");
         radio = input.nextFloat();
-        
-        area = (float) (Math.PI * Math.pow(radio , 2));
-    
-return area;
-}
-   
-   public static float showFormulaOfDistance(float distance){
-       Scanner input = new Scanner (System.in);
-       float initialvelocity;
-       float time;
-       float aceleration; 
+        areaofcircle = computeCircleArea(area, radio);
+        System.out.println("the area of circle is ->" + areaofcircle);
+
+        System.out.println("FOMULA OF DISTANCE MRUV");
         System.out.println(" enter initial velocity --> ");
         initialvelocity = input.nextInt();
         System.out.println(" enter time --> ");
         time = input.nextInt();
         System.out.println(" enter initial aceleration --> ");
         aceleration = input.nextInt();
-        
-        distance= (float) (initialvelocity * time + 1/2 * aceleration * Math.pow(time, 2));
-        
-return distance;
- 
-  }
-   public static float showFomulaOfForce(float force){
-        Scanner input = new Scanner (System.in);
-        float mass;
-        float aceleration;
-        
+        distance = showFormulaOfDistance(distance, initialvelocity, time, aceleration);
+        System.out.println("the distance traveled is --> " + distance);
+
+        System.out.println("FORMULA OF FORCE");
         System.out.println(" enter mass --> ");
         mass = input.nextInt();
         System.out.println(" enter initial aceleration --> ");
         aceleration = input.nextInt();
+        force = showFomulaOfForce(force, mass, aceleration);
+        System.out.println(" the force is -->" + force);
+
+        System.out.println("FORMULA OF WORK");
+        System.out.println(" enter force --> ");
+        forces = input.nextInt();
+        System.out.println(" enter distance --> ");
+        distances = input.nextInt();
+        work = showFomulaOfWork(work, forces, distances);
+        System.out.println(" the work is -->" + work);
+
+    }
+
+    public static void printBanner() {
+        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n"
+                + "|             UNIVERSIDAD DE LAS FUERZAS ARMADAS              |\n"
+                + "|                         (ESPE)                              |\n"
+                + "| Programming Fundamentals                                    |\n"
+                + "| Programer: ERICK OCHOA                                      |\n"
+                + "| WS13 TASK                                                   |\n"
+                + "| CALCULATE TAXES                                             |\n"
+                + "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n");
+    }
+
+    public static float computeTrapezoidArea(float area, float majorbase, float minorbase, float altitude) {
+        area = ((majorbase + minorbase) / 2) * altitude;
+        return area;
+    }
+
+    public static float computeRegularPolygonArea(float area, float numberofsides, float sides, float perimeter, float apothem) {
+        perimeter = numberofsides * sides;
+        area = (perimeter * apothem) / 2;
+        return area;
+    }
+
+    public static float computeCircleArea(float area, float radio) {
+        area = (float) (Math.PI * Math.pow(radio, 2));
+
+        return area;
+    }
+
+    public static float showFormulaOfDistance(float distance, float initialvelocity, float time, float aceleration) {
+        distance = (float) (initialvelocity * time + 1 / 2 * aceleration * Math.pow(time, 2));
+
+        return distance;
+
+    }
+
+    public static float showFomulaOfForce(float force, float mass, float aceleration) {
         force = mass * aceleration;
         return force;
-   }
-   public static float showFomulaOfWork(float work){
-        Scanner input = new Scanner (System.in);
-        float force;
-        float distance;
-        
-        System.out.println(" enter force --> ");
-        force = input.nextInt();
-        System.out.println(" enter distance --> ");
-        distance = input.nextInt();
-        work = force * distance;
+    }
+
+    public static float showFomulaOfWork(float work, float forces, float distances) {
+        work = forces * distances;
         return work;
-   
-   
-   } 
+
+    }
 }
-
-
-
